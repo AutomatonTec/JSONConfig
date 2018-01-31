8b8371fca4c670c7aba9410f4af2e54e1c84b3f8
Rockford Wei <rocky@perfect.org>
2018-01-31 11:09:14 -0500

    Updating URL.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     name: "JSONConfig",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 3),
     ]
 )
